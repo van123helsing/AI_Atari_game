23ba19961f4bd4815c4a1bb9280b1db5ff9470db
ŠMID <js8649@student.uni-lj.si>
2020-05-12 19:34:19 +0200

    Izbira trainer/solver

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ import gym
 import numpy as np
 from game_models.ddqn_game_model import DDQNTrainer, DDQNSolver
 from gym_wrappers import MainGymWrapper
-import time
+import sys
 
 FRAMES_IN_OBSERVATION = 4
 FRAME_SIZE = 84
@@ -10,7 +10,6 @@ INPUT_SHAPE = (FRAMES_IN_OBSERVATION, FRAME_SIZE, FRAME_SIZE)
 
 
 def _main_loop(game_model, env):
-
     run = 0
     total_step = 0
     while True:
@@ -28,7 +27,7 @@ def _main_loop(game_model, env):
             step += 1
 
             # graificni prikaz
-            # env.render()
+            env.render('human')
 
             action = game_model.move(current_state)
             next_state, reward, done, info = env.step(action)
@@ -36,15 +35,12 @@ def _main_loop(game_model, env):
             # normaliziramo nagrado
             np.sign(reward)
             score += reward
+
             # shranimo
-			
             game_model.remember(current_state, action, reward, next_state, done)
             current_state = next_state
 
-            tic = time.perf_counter()
             game_model.step_update(total_step)
-            toc = time.perf_counter()
-            print(format(toc - tic, '.2f'))
 
             if done:
                 game_model.save_run(score, step, run)
@@ -53,9 +49,16 @@ def _main_loop(game_model, env):
 def main():
     game_name = "Breakout-v0"
     env = MainGymWrapper.wrap(gym.make(game_name))
-    _main_loop(DDQNTrainer(game_name, INPUT_SHAPE, env.action_space.n), env)
-
-    # DDQNSolver(game_name, INPUT_SHAPE, action_space)
+    if len(sys.argv) > 1:
+        AB = sys.argv[1]
+        if AB == 'A':
+            _main_loop(DDQNTrainer(game_name, INPUT_SHAPE, env.action_space.n), env)
+        elif AB == 'B':
+            _main_loop(DDQNSolver(game_name, INPUT_SHAPE, env.action_space.n), env)
+        else:
+            print("Kot parameter morate vnesti eno izmed črk (A/B)")
+    else:
+        print("Kot parameter morate vnesti eno izmed črk (A/B)")
 
 
 if __name__ == "__main__":
